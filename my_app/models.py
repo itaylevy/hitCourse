dca0aeddbc54daae39e65de039f7f14306638c1b
Itay Levy <levyetay@gmail.com>
2021-05-17 20:23:10 +0300

    add package table to db

--- my_app/models.py
+++ my_app/models.py
@@ -6,3 +6,6 @@ class Package(models.Model):
     package_price = models.CharField(max_length=4)
     package_duration = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.package_name
+
--- my_app/views.py
+++ my_app/views.py
@@ -1,21 +1,11 @@
 from django.shortcuts import render
-
-communication_packages = [
-    {'package_name': 'triple',
-     'package_price': '159',
-     'package_duration': 'year'},
-    {'package_name': 'tv_only',
-     'package_price': '79',
-     'package_duration': 'year'},
-    {'package_name': 'internet_only',
-     'package_price': '99',
-     'package_duration': 'year'},
-]
+from .models import Package
 
 
 def home(request):
     context = {
-        'packages': communication_packages,
+        # 'packages': communication_packages,
+        'packages': Package.objects.all(),
         'title': 'Home page'
     }
     return render(request, 'my_app/index.html', context)
