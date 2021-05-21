edda7d959116da2669feccd9289246c85dfae56f
Itay Levy <levyetay@gmail.com>
2021-05-21 21:19:49 +0300

    move to environment variables

--- hitCourse/settings.py
+++ hitCourse/settings.py
@@ -18,14 +18,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
+#'5ff582e1aae5022d85e81d989dfde094e2da8206f43476fa'
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ld%md$6&n0px2ar)m@rdz=be0ktwd89ggkwkl&tt^ckz9=xtxd'
+# SECRET_KEY = 'django-insecure-ld%md$6&n0px2ar)m@rdz=be0ktwd89ggkwkl&tt^ckz9=xtxd'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG_MODE_DJANGO')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['communicationililtd.herokuapp.com']
 
 
 # Application definition
@@ -148,8 +149,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'communicationililtd@gmail.com'
-# EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
-EMAIL_HOST_PASSWORD = 'itayliorilia'
-# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
