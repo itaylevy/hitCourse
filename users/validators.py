05bf1ce767cd0e32f6918568f752c4bdb61b8c3d
Itay Levy <levyetay@gmail.com>
2021-05-30 23:30:54 +0300

    added lower upper and number validatior to password

--- users/validators.py
+++ users/validators.py
@@ -23,22 +23,35 @@ class UniqueSymbolsValidator:
 
 
 class LetterCaseValidator:
-    def __init__(self, upper_case_letters=string.ascii_uppercase[:26], lower_case_letters=string.ascii_lowercase[:26]):
+    def __init__(self, upper_case_letters=tuple(string.ascii_uppercase[:26]), lower_case_letters=tuple(string.ascii_lowercase[:26]), numbers=range(0, 10)):
         self.upper_case_letters = upper_case_letters
         self.lower_case_letters = lower_case_letters
+        self.numbers = numbers
 
     def validate(self, password, user=None):
-        if re.search(self.lower_case_letters, password) is None:
-            print("Visit first")
+        lower_case_letter_exist = False
+        for lower_case_letter in self.lower_case_letters:
+            if lower_case_letter in password:
+                lower_case_letter_exist = True
+                break
+        upper_case_letter_exist = False
+        for upper_case_letter in self.upper_case_letters:
+            if upper_case_letter in password:
+                upper_case_letter_exist = True
+                break
+        number_exist = False
+        for number in self.numbers:
+            if str(number) in password:
+                number_exist = True
+                break
+        if not lower_case_letter_exist:
             raise ValidationError(message="Password must contain at least one lowercase letter")
-        if re.search(self.upper_case_letters, password) is None:
-            print("Visit second")
+        if not upper_case_letter_exist:
             raise ValidationError(message="Password must contain at least one uppercase letter")
-
-
-
+        if not number_exist:
+            raise ValidationError(message="Password must contain at least one number")
 
     def get_help_text(self):
         return _(
-            "Password must contain at least one uppercase letter and one lower case letter"
+            "Your password must contain at least one uppercase letter and one lower case letter and one number"
         )
